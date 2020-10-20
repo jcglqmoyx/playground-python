70feb23b285049bffbee41f606582b35feecb026
Chintsai Hwo <jcglqmoyx@gmail.com>
2020-10-20 12:03:47 +0800

    update

--- src/retrieve_hanja_to_hangul_dataset.py
+++ src/retrieve_hanja_to_hangul_dataset.py
@@ -6,7 +6,11 @@ chrome_options = webdriver.ChromeOptions()
 browser = webdriver.Chrome()
 url = "https://hanja.dict.naver.com/"
 browser.get(url)
-browser.find_element_by_id('autoCompl_Input').send_keys('你')
+hanja_input_id = 'autoCompl_Input'
+hangul_output_class = 'kor_tone'
+browser.find_element_by_id(hanja_input_id).send_keys('你')
 time.sleep(2)
-browser.find_element_by_id('autoCompl_Input').click()
+browser.find_element_by_id(hanja_input_id).click()
+res = browser.find_elements_by_class_name(hangul_output_class)[0].value
+print(res)
 # browser.quit()
